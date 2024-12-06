fd880ed8b24363fc620321f448aeb47e25ff6742
Ian Pollack <ianjpollack@gmail.com>
2024-12-05 17:13:45 -0800

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from "./components/CalendarApp";
+import "./styles/CalendarApp.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  )
+}
+
+export default App
